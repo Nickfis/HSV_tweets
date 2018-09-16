545358a5f980c87b66ba8b94178ef1deaa5f564d
Nickfis <niklas.fischer@barcelonagse.eu>
2018-09-16 21:16:09 +0200

    updated preprocessing

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 import os
+import matplotlib as plt
 
 os.chdir("/home/Nickfis/Documents/Projects/HSV_tweets")
 
 np.random.seed(200)
 # reading in training set
-train = pd.read_csv('trainingset.csv', engine='python', header=None, encoding='ISO-8859–1').sample(1500)
+train = pd.read_csv('trainingset.csv', engine='python', header=None, encoding='ISO-8859–1').sample(10000)
 
 train.head()
 ############################################### data preprocessing: Cleaning up ###############################################
@@ -22,7 +23,7 @@ train['length'].describe()
 # for the tweets that have above 140 characters, we can see that there are problems with html encoding.
 train[train['length']>140]
 # checking for tweet 569987
-train.loc[852964].tweet
+train.loc[972155].tweet
 # we have to clean this up.Furthermore mentions, urls and hashtags will be removed in the cleaning process
 # example1 = BeautifulSoup(train.loc[569987].tweet, 'lxml')
 # # way better.
@@ -89,10 +90,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer()
 cv.fit(train.tweet)
 
+neg_cv
+
 # looking at the use of each of the words found split up between tweets labelled as positive and negative
+# transofrm function to turn the sparse matrix cv into a dense one
 neg_cv = cv.transform(train[train['sentiment'] == 0].tweet) # 0 encoded as negative sentiment
 pos_cv = cv.transform(train[train['sentiment'] == 4].tweet) # 4 encoded as positive
-neg_tf = np.sum(neg_cv,axis=0)
+neg_tf = np.sum(neg_cv,axis=0) # summing over all observed values
 pos_tf = np.sum(pos_cv,axis=0)
 neg = np.squeeze(np.asarray(neg_tf))
 pos = np.squeeze(np.asarray(pos_tf))
@@ -105,9 +109,16 @@ frequency.columns = ['negative', 'positive']
 frequency['overall_frequency'] = frequency['negative'] + frequency['positive']
 # sort by overall frequency to see the most used words in the dataset
 frequency.sort_values(by='overall_frequency', ascending=False)
+# positive rate: how often is a word used in a positive tweet to overall mentions? Use it as feature in later model
+frequency['pos_rate'] = frequency['positive'] / frequency['overall_frequency']
+# what's the distribution of our words?
+frequency['overall_frequency'].describe()
+frequency['overall_frequency'].hist(bins=100, range=(0,50))
 
-train.head()
-
+# look at top 10 words most used in positive and negative tweets
+frequency.head()
+frequency.sort_values(by='negative', ascending=False).head(10)
+frequency.sort_values(by='positive', ascending=False).head(10)
 # pos_tagging: first part of speech tagging for stemming of the used words in the tweets
 import nltk
 # tokenization
@@ -118,6 +129,8 @@ def tokenizer(tweet):
 # example what we have to do now in order to find stem for each word
 text = tokenizer(train['tweet'].iloc[3])
 nltk.pos_tag(text2)
+
+## adding more and more features to the analysis for the classifier first. # analysis part 3 has to be finished.
 
 
 5+3
